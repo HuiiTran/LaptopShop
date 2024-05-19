4ccdc5309a88b25024f3998ffbda9c9ed99e7807
Trần Quang Huy <huy.com2003@gmail.com>
2024-05-19 20:02:13 +0700

    Add Product List

---
+++ LaptopShop_Project/App.js
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+export default function App(){
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
---
+++ LaptopShop_Project/Screens/ProductListScreen.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  View,
+  FlatList,
+  Platform,
+  Button,
+  ActivityIndicator,
+  StyleSheet,
+  Text
+} from "react-native";
+
+const ProductListScreen = props => {
+    return(
+        <Text>ProductList</Text>
+    );
+}
+
+export default ProductListScreen;
