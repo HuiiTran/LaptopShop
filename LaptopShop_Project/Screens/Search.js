a60248c4a05e0b813e93eaa7b841e771bf2fdbde
Trần Quang Huy <huy.com2003@gmail.com>
2024-06-07 22:13:28 +0700

    Implement Search

--- LaptopShop_Project/Screens/Search.js
+++ LaptopShop_Project/Screens/Search.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+/* eslint-disable prettier/prettier */
+import React, {useState, useEffect} from 'react'
 import {
     ScrollView,
     StyleSheet,
@@ -16,17 +17,7 @@ import CustomInput from '../Components/CustomInputField'
 import searchIcon from '../assets/icons/ion_search.png'
 import sortIcon from '../assets/icons/Sort.png'
 
-class Product {
-    constructor(id, ownerId, title, imageUrl, description, preprice, price) {
-      this.id = id;
-      this.ownerId = ownerId;
-      this.title = title;
-      this.imageUrl = imageUrl;
-      this.description = description;
-      this.preprice = preprice;
-      this.price = price;
-    }
-  }
+import { ProjectBaseUrl } from './Home';
 
 const Search = (props) => {
     const handleInputChange = (text) => {
@@ -34,122 +25,45 @@ const Search = (props) => {
       };
 
     const [searchText,setSearchText] = useState('');
-    const [products, setProducts] = useState([
-        new Product(
-          'p1',
-          'u1',
-          'Red Shirt',
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-          'A red t-shirt, perfect for days with non-red weather.',
-          120000,
-          100000
-        ),
-        new Product(
-          'p2',
-          'u1',
-          'Blue Carpet',
-          'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-          'Fits your red shirt perfectly. To stand on. Not to wear it.',
-          120.00,
-          99.99
-        ),
-        new Product(
-          'p3',
-          'u2',
-          'Coffee Mug',
-          'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
-          'Can also be used for tea!',
-          10,
-          8.99
-        ),
-        new Product(
-          'p4',
-          'u3',
-          'The Book - Limited Edition',
-          'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
-          "What the content is? Why would that matter? It's a limited edition!",
-          20,
-          15.99
-        ),
-        new Product(
-          'p5',
-          'u3',
-          'PowerBook',
-          'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
-          'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
-          2300,
-          2299.99
-        ),
-        new Product(
-          'p6',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          10,
-          5.49
-        ),
-        new Product(
-          'p7',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          10,
-          5.49
-        ),
-        new Product(
-          'p8',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          10,
-          
-          5.49
-        ),
-        new Product(
-          'p9',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          10,
-      
-          5.49
-        ),
-        new Product(
-          'p10',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          
-          10,
-          5.49
-        ),
-        new Product(
-          'p11',
-          'u1',
-          'Pen & Paper',
-          'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-          "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-          
-          10,
-          5.49
-        )
-]);
-    const handleSearch = (text) => {
-        setSearchText(text);
-
-        const filteredProducts = products.filter((product) =>
-            product.title.toLowerCase().includes(text.toLowerCase())
-        );
-
-        const sortedProducts = filteredProducts.sort((a, b) => a.title.localeCompare(b.title));
-
-        setProducts(sortedProducts);
+    const [data, setData] = useState([{
+      id: String,
+      storeID: String,
+      classify: String,
+      name: String,
+      description: String,
+      price: Number,
+      quantity: Number,
+      isAvailable: Boolean,
+      image: [String],
+    }]);
+    const [filterData, setFilterData] = useState();
+  const getList = async () => {
+    try {
+      fetch(ProjectBaseUrl + '/catalog-gateway/items')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setData(responseJson);
+        setFilterData(responseJson);
+        //console.log(responseJson);
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
+  useEffect(() => {
+    getList();
+  },[]);
+  useEffect(() => {
+
+    const filtered = data.filter(item =>
+      item.name.toString().toLowerCase().includes(searchText.toLowerCase()),
+    );
+    if (searchText === '') {
+      return setFilterData(data);
+    }
+
+    setFilterData(filtered);
+  },[searchText]);
 
     const handleSortPress = () => {
       // Your logic for handling sort  here
@@ -171,38 +85,49 @@ const Search = (props) => {
         </View>
         
         <View style={styles.container}>
-          <CustomInput
-            placeholderImage={searchIcon}
+          <Image source={searchIcon} />
+          <TextInput
+            style={styles.textInputSearch}
             value={searchText}
-            onChangeText={handleSearch}
-            placeholderText="Search"
-          ></CustomInput>
+            onChangeText={text => {setSearchText(text);}}
+            placeholder='Search'
+            placeholderColor="#c4c3cb"
+          ></TextInput>
 
-          <TouchableOpacity onPress={handleSortPress}>
+          {/* <TouchableOpacity onPress={handleSortPress}>
             <Image
             source={sortIcon}
             style={styles.sortImage}
             />
-          </TouchableOpacity>
+          </TouchableOpacity> */}
         </View>
-        <FlatList
-            style={{marginLeft:10}}
-            data={products}
+        {
+          filterData?.length !== 0 ? 
+          <FlatList
+            style={{marginLeft:10, marginTop:10}}
+            data={filterData}
             horizontal={false}
             numColumns={2}
-            keyExtractor={(item) => item.id}
-            renderItem={(itemData) => (
-              <ProductItem
-                image={itemData.item.imageUrl}
-                title={itemData.item.title}
-                preprice={itemData.item.preprice}
-                price={itemData.item.price}
+            keyExtractor={({id}) => id}
+            renderItem={({item}) => (
+            <ProductItem
+                image={item.image}
+                title={item.name}
+                price={item.price}
                 onSelect={() => {
-                  props.navigation.navigate('ProductDetailScreen');
-                }}
-              />
-            )}
-          />
+                  props.navigation.navigate('ProductDetailScreen', {itemId: item.id});
+          }}>
+          
+            </ProductItem>
+          )}/>
+          :
+          (
+            <View>
+              <Text>No result</Text>
+            </View>
+          )
+        }
+        
     </ScrollView>
     
   )
@@ -224,7 +149,8 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-between',
         alignItems:'center',
-        padding:10
+        padding:10,
+        marginLeft: 10,
     },
     logo:{
         paddingTop:7,
@@ -264,6 +190,18 @@ const styles = StyleSheet.create({
       resizeMode: 'contain',
           height: 50,
           width: 50,
+    },
+    textInputSearch: {
+      height: 43,
+      fontSize: 14,
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor: '#eaeaea',
+      backgroundColor: '#fafafa',
+      paddingLeft: 10,
+      marginTop: 5,
+      marginBottom: 5,
+      width: 350,
     },
 })
 
